d106e3e5a7dcd5e46f17c031bcc0dab253e4d720
ftovar8 <felix.godo@gmail.com>
2023-04-19 09:45:00 -0400

    feat: templates and export templates entries

---
+++ templates/projects/web-backend-app/modules/layouts/menu/ts/controller.ts
@@ -0,0 +1,9 @@
+import {ReactWidgetController} from '@beyond-js/react-18-widgets/base';
+import {Menu} from "./views";
+
+export /*bundle*/
+class Controller extends ReactWidgetController {
+    get Widget() {
+        return Menu;
+    }
+}
