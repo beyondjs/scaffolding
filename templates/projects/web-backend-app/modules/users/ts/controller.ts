9aacbf9623dd461b33e74f6f5bd9cb218416280d
ftovar8 <felix.godo@gmail.com>
2023-07-28 20:03:52 -0300

    refactor: :art: update templates and packages versions

--- templates/projects/web-backend-app/modules/users/ts/controller.ts
+++ templates/projects/web-backend-app/modules/users/ts/controller.ts
@@ -1,9 +1,25 @@
-import {PageReactWidgetController} from '@beyond-js/react-18-widgets/page';
-import {Page} from "./views";
+import { PageReactWidgetController } from '@beyond-js/react-18-widgets/page';
+import { StoreManager } from './store';
+import { View } from './views';
 
 export /*bundle*/
 class Controller extends PageReactWidgetController {
-    get Widget() {
-        return Page;
-    }
+	#store: StoreManager;
+	createStore() {
+		this.#store = new StoreManager();
+		return this.#store;
+	}
+	get Widget() {
+		return View;
+	}
+
+	/**
+	 * this method is executed when the widget is showd
+	 */
+	load() {}
+
+	/**
+	 * this method is executed when the widget is hidden
+	 */
+	hide() {}
 }
