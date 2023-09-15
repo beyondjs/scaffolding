268c48301d368d03c8f8f483fa01c3eccacc1671
veD-tnayrB <Brayanmc.contacto@gmail.com>
2023-09-14 21:30:08 -0400

    Feature: Put the base of the new templates

---
+++ templates/bundles/page/vue/promotions.vue
@@ -0,0 +1,36 @@
+<template>
+	<div className="promotion">
+		<div className="logos">
+			<BeyondSVG />
+			<SvelteSVG />
+		</div>
+
+		<h2>BeyondJS + Vue</h2>
+	</div>
+</template>
+
+<script lang="ts">
+import BeyondSVG from './beyond.vue';
+import SvelteSVG from './vue.vue';
+
+export default {
+	components: {
+		BeyondSVG,
+		SvelteSVG,
+	},
+};
+</script>
+
+<style lang="scss">
+h2 {
+	font-size: 2.5rem;
+	text-align: center;
+}
+
+.logos {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 1.5rem;
+}
+</style>
